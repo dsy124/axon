b3eaeb09a3a6268cbddc3d820c05963ef8b8dafb
Eason <kaoimin@qq.com>
2023-02-27 09:59:37 +0800

    feat: add dummy input mode of ckb cell verify

--- protocol/src/lazy.rs
+++ protocol/src/lazy.rs
@@ -1,8 +1,14 @@
 use arc_swap::ArcSwap;
+use ckb_types::{packed, prelude::*};
 
-use crate::types::MerkleRoot;
+use crate::types::{Hasher, MerkleRoot, H256};
 
 lazy_static::lazy_static! {
     pub static ref CURRENT_STATE_ROOT: ArcSwap<MerkleRoot> = ArcSwap::from_pointee(Default::default());
     pub static ref CHAIN_ID: ArcSwap<u64> = ArcSwap::from_pointee(Default::default());
+    pub static ref CELL_VERIFIER_CODE_HASH: H256 = Hasher::digest("AxonCellVerifier");
+    pub static ref DUMMY_INPUT_OUT_POINT: packed::OutPoint
+        = packed::OutPointBuilder::default()
+            .tx_hash(Hasher::digest("DummyInputOutpointTxHash").0.pack())
+            .index(0u32.pack()).build();
 }
